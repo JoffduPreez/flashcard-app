5bf1c9121a43bb0fec0a0ee8f08722738698712c
JoffduPreez <joffre@joffredupreez.com>
2023-12-24 14:23:53 -0500

    Initial project commit

---
+++ src/test/java/com/joffredupreez/flashcardapp/FlashcardWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joffredupreez.flashcardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
